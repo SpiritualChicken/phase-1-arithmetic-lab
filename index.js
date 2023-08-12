5f2448969d46d66c76a382e55836b4d59e7deff2
Charlie <cnewlinds@gmail.com>
2023-08-12 16:51:51 +1000

    Math update

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+let multiply = 8 * 8;
+
+console.log(multiply);
+
+let random = Math.random() + 1;
+
+console.log(random);
+
+let mod = 4 % 10; 
+
+console.log(mod);
+
+let max = Math.max(1, 2, 3, 4, 10, 20);
+
+console.log(max);
